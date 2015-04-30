5a51e866a8b4cfcc1fcfa9035c358f8de0f9dc0b
Matias Singers <mail@matiassingers.com>
2015-04-30 19:14:32 +0800

    Return early on errors

--- server.js
+++ server.js
@@ -14,45 +14,53 @@ app.use(function *(){
   var self = this;
   var query = this.request.query;
   var id = query.id;
-  if (id){
-    var result = yield request({
-      uri: 'http://datamall2.mytransport.sg/ltaodataservice/BusArrival?BusStopID=' + id,
-      json: true,
-      headers: {
-        AccountKey: nconf.get('accountKey'),
-      	UniqueUserId: nconf.get('uniqueUserID')
-      }
-    });
-    if (result.statusCode == 200 && result.body){
-      var services = result.body.Services.map(function(service){
-        var nextArrival = service.NextBus.EstimatedArrival;
-        var subsequentArrival = service.SubsequentBus.EstimatedArrival;
-        var now = Date.now();
-        return {
-          no: service.ServiceNo,
-          next: {
-            time: nextArrival,
-            duration_ms: nextArrival ? (new Date(nextArrival) - now) : null
-          },
-          subsequent: {
-            time: subsequentArrival,
-            duration_ms: subsequentArrival ? (new Date(subsequentArrival) - now) : null
-          }
-        }
-      });
-      this.body = {
-        services: services
-      };
-    } else {
-      this.body = {
-        error: 'Invalid bus stop ID provided.'
-      };
-    }
-  } else {
+
+  if (!id){
     this.body = {
       error: 'Bus stop ID is not provided.'
     };
+
+    return;
   }
+
+  var result = yield request({
+    uri: 'http://datamall2.mytransport.sg/ltaodataservice/BusArrival?BusStopID=' + id,
+    json: true,
+    headers: {
+      AccountKey: nconf.get('accountKey'),
+    	UniqueUserId: nconf.get('uniqueUserID')
+    }
+  });
+
+  if (result.statusCode !== 200 || !result.body){
+    this.body = {
+      error: 'Invalid bus stop ID provided.'
+    };
+
+    return;
+  }
+
+  var services = result.body.Services.map(function(service){
+    var nextArrival = service.NextBus.EstimatedArrival;
+    var subsequentArrival = service.SubsequentBus.EstimatedArrival;
+    var now = Date.now();
+    
+    return {
+      no: service.ServiceNo,
+      next: {
+        time: nextArrival,
+        duration_ms: nextArrival ? (new Date(nextArrival) - now) : null
+      },
+      subsequent: {
+        time: subsequentArrival,
+        duration_ms: subsequentArrival ? (new Date(subsequentArrival) - now) : null
+      }
+    }
+  });
+
+  this.body = {
+    services: services
+  };
 });
 
 app.listen(nconf.get('PORT') || nconf.get('port'));
