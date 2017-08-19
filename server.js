8a4287f4343b8afbec82bc3434e8cf0619630a05
Lim Chee Aun <cheeaun@gmail.com>
2017-08-20 00:41:09 +0800

    Replace `got` with `node-fetch` and `async-retry`

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const Koa = require('koa');
 const cors = require('kcors');
-const got = require('got');
+const retry = require('async-retry')
+const fetch = require('node-fetch');
 const cache = require('lru-cache')({
   maxAge: 1000 * 15 // 15 seconds
 });
@@ -30,49 +31,55 @@ app.use(async (ctx) => {
   if (!services){
     const url = 'http://datamall2.mytransport.sg/ltaodataservice/BusArrival?BusStopID=' + id;
     console.log('Fetch ' + url);
-    const result = await got(url, {
-      json: true,
-      timeout: 1000 * 10, // 10 seconds
-      headers: {
-        AccountKey: process.env.accountKey,
-      },
-    });
-
-    if (result.statusCode !== 200 || !result.body){
-      ctx.body = {
-        error: 'Invalid bus stop ID provided.'
-      };
-      return;
-    }
-
-    services = result.body.Services.map((service) => {
-      const { NextBus, SubsequentBus } = service;
-      const nextArrival = NextBus.EstimatedArrival;
-      const subsequentArrival = SubsequentBus.EstimatedArrival;
-      const now = Date.now();
-
-      return {
-        no: service.ServiceNo,
-        next: {
-          time: nextArrival,
-          duration_ms: nextArrival ? (new Date(nextArrival) - now) : null,
-          lat: parseFloat(NextBus.Latitude, 10),
-          lng: parseFloat(NextBus.Longitude, 10),
-          load: NextBus.Load,
-          feature: NextBus.Feature,
+    await retry(async () => {
+      const result = await fetch(url, {
+        timeout: 1000 * 10, // 10 seconds
+        headers: {
+          AccountKey: process.env.accountKey,
         },
-        subsequent: {
-          time: subsequentArrival,
-          duration_ms: subsequentArrival ? (new Date(subsequentArrival) - now) : null,
-          lat: parseFloat(SubsequentBus.Latitude, 10),
-          lng: parseFloat(SubsequentBus.Longitude, 10),
-          load: SubsequentBus.Load,
-          feature: SubsequentBus.Feature,
-        }
-      }
-    });
+      }).then(async (res) => ({
+        statusCode: res.status,
+        body: await res.json(),
+      }));
 
-    cache.set(id, services);
+      if (result.statusCode !== 200 || !result.body){
+        ctx.body = {
+          error: 'Invalid bus stop ID provided.'
+        };
+        return;
+      }
+
+      services = result.body.Services.map((service) => {
+        const { NextBus, SubsequentBus } = service;
+        const nextArrival = NextBus.EstimatedArrival;
+        const subsequentArrival = SubsequentBus.EstimatedArrival;
+        const now = Date.now();
+
+        return {
+          no: service.ServiceNo,
+          next: {
+            time: nextArrival,
+            duration_ms: nextArrival ? (new Date(nextArrival) - now) : null,
+            lat: parseFloat(NextBus.Latitude, 10),
+            lng: parseFloat(NextBus.Longitude, 10),
+            load: NextBus.Load,
+            feature: NextBus.Feature,
+          },
+          subsequent: {
+            time: subsequentArrival,
+            duration_ms: subsequentArrival ? (new Date(subsequentArrival) - now) : null,
+            lat: parseFloat(SubsequentBus.Latitude, 10),
+            lng: parseFloat(SubsequentBus.Longitude, 10),
+            load: SubsequentBus.Load,
+            feature: SubsequentBus.Feature,
+          }
+        }
+      });
+
+      cache.set(id, services);
+    }, {
+      retries: 3,
+    });
   }
 
   ctx.body = {
