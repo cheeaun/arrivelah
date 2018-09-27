357b97a12911490126d02806f1a2dc9b7ee999b8
Lim Chee Aun <cheeaun@gmail.com>
2018-09-27 08:37:19 +0800

    Remove ‘localhost’ text

--- server.js
+++ server.js
@@ -89,4 +89,4 @@ app.use(async (ctx) => {
 
 const port = process.env.PORT || 8081;
 app.listen(port);
-console.log('Server started at localhost:' + port);
+console.log('Server started at port ' + port);
